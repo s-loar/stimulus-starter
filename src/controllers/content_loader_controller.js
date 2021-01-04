3e619a8e27bdf1cc7459adb5d4ea5c76d6f6781e
Steven Loar <sjl7909@gmail.com>
2021-01-04 16:41:38 -0500

    Async fetch of HTML and display, refetch data every 5 seconds

---
+++ src/controllers/content_loader_controller.js
@@ -0,0 +1,36 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static values = { url: String, refreshInterval: Number }
+
+  startRefreshing() {
+    this.refreshTimer = setInterval(() => {
+      this.load()
+    }, this.refreshIntervalValue)
+  }
+
+  stopRefreshing() {
+    if (this.refreshTimer) {
+      clearInterval(this.refreshTimer)
+    }
+  }
+
+  connect() {
+    this.load()
+
+    if (this.hasRefreshIntervalValue) {
+      this.startRefreshing()
+    }
+  }
+
+  disconnect() {
+    this.stopRefreshing()
+  }
+
+  load() {
+    console.log("fetching")
+    fetch(this.urlValue)
+      .then(response => response.text())
+      .then(html => this.element.innerHTML = html)
+  }
+}
